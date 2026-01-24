dd678c80f466217c5d2fce70002a5c79df7bf727
Seankariya <sean.kariya@gmail.com>
2026-01-24 13:51:26 -0500

    Initial Commit

---
+++ src/main/java/frc/robot/commands/runAll.java
@@ -0,0 +1,64 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Constants.HopperConstants;
+import frc.robot.Constants.shooterConstants;
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Feeder;
+import frc.robot.subsystems.Hopper;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class runAll extends Command {
+  private final Feeder feederSubsystem;
+  private final Shooter shooterSubsystem;
+  private final Hopper hopperSubsystem;
+
+  ShuffleboardTab tab1 = Shuffleboard.getTab("feederallMotor");
+  GenericEntry pEntry = tab1.add("SET allFEEDSPEED", shooterConstants.feederspeed).getEntry();
+  ShuffleboardTab tab2 = Shuffleboard.getTab("shooterallMotor");
+  GenericEntry dEntry = tab2.add("SET allSPEED", shooterConstants.speed).getEntry();
+  ShuffleboardTab tab3 = Shuffleboard.getTab("HopperallMotor");
+  GenericEntry iEntry = tab3.add("SET allHOPPERSPEED", HopperConstants.hopperSpeed).getEntry();
+
+    public runAll(Feeder feederSubsystem, Shooter shooterSubsystem, Hopper hopperSubsystem) {
+      this.feederSubsystem = feederSubsystem;
+      this.shooterSubsystem = shooterSubsystem;
+      this.hopperSubsystem = hopperSubsystem;
+    
+  }
+
+  // Called when the command is initially scheduled.
+
+
+  @Override
+  public void initialize() {
+    feederSubsystem.setIdle();
+    shooterSubsystem.setIdle();
+    hopperSubsystem.setIdle();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    shooterSubsystem.setMotorSpeed(dEntry.getDouble(0.1));
+    new WaitCommand(1);
+    feederSubsystem.setFeederSpeed(pEntry.getDouble(0.1));
+    hopperSubsystem.setSpeed(iEntry.getDouble(0.1));
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    feederSubsystem.setIdle();
+    shooterSubsystem.setIdle();
+  }
+
+}
